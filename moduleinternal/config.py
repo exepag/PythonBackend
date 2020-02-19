94020ec184e64e30f073adc97e0a5c80cbf51023
exe <exepag@gmail.com>
2020-02-20 02:58:07 +0700

    18.139.222.3

--- moduleinternal/config.py
+++ moduleinternal/config.py
@@ -8,6 +8,6 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'qwer'
 app.config['MYSQL_DATABASE_DB'] = 'customer'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
-HOST_SERVER = "http://18.141.8.140"
-PORT_SERVICE = "9803"
+HOST_SERVER = "http://18.139.222.3"
+PORT_SERVICE = "9805"
 HOST_SERVER_SERVICE = HOST_SERVER+":"+str(PORT_SERVICE)
